8a9421c4bda50ec847043ff0b569e02918d82adc
UA-X99 <ua-x99@hotmail.com>
2025-04-29 01:53:46 -0600

    Reestructurando proyecto en carpeta netcore

--- netcore/ExploringAccions/obj/Debug/net8.0/ExploringAccions.AssemblyInfo.cs
+++ netcore/ExploringAccions/obj/Debug/net8.0/ExploringAccions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExploringAccions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dfc121bc3b6bb7a92657836eb5a8977f517bb67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a140149a8a65b73925f19b9ae98acbedc28c80c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExploringAccions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExploringAccions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
